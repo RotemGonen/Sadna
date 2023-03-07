78a2074091ffbe66e64978e369620e0fedbd6ebc
omershik <omer41198@gmail.com>
2023-03-07 22:39:11 +0200

    adding connection between the tables

--- loginpage.php
+++ loginpage.php
@@ -16,24 +16,23 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $login_time = date('Y-m-d H:i:s');
 
-if (isset($_POST['submit'])) {
+// Query the registration table to see if the username and password exists
+$sql_user = "SELECT * FROM registrations WHERE username='$username'";
+$result_user = $conn->query($sql_user);
+$sql_pass = "SELECT * FROM registrations WHERE password='$password'";
+$result_pass = $conn->query($sql_user);
 
+// If the username and password exists, check the login table for a matching email and password
+if ($result_user->num_rows > 0 && $result_pass->num_rows > 0) {
+    echo "User is authenticated";
+    echo "<br>";
     // perform the database insertion
     $sql = "INSERT INTO logins (username, password, login_time)
     VALUES ('$username', '$password', '$login_time')";
-    // if (mysqli_query($conn, $sql)) {
-    //     echo "login successful";
-    // } else {
-    //     echo "Error: " . mysqli_error($conn);
-    // }
-    if ($conn->query($sql)==FALSE){
-        echo "Can not login. Error is: ".
-            $conn->error;
-        }
-    else
-        echo "login succeesful";
-}
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    if (mysqli_query($conn, $sql)) {
+        echo "login successful"; }
+ }
+else 
+    echo "Incorrect email or password";
 
-?>
+ ?>
