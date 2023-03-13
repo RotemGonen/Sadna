41757eeabdc7234955521a674895aa1257e14ee5
rotem <bgdogname@gmail.com>
2023-03-13 10:54:28 +0200

    adding map

--- reservefield.php
+++ reservefield.php
@@ -21,6 +21,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
 
 </head>
 
@@ -73,32 +77,48 @@
         </div>
 
         <div class="container">
-            <div class="col">
-                <!-- search city bar -->
-                <select class="form-control" id="location-search" style="width: 100%">
-                </select>
-                <!-- search type bar -->
-                <select class="form-control" id="type-select" style="width: 100%">
-                    <option value="אולם ספורט">אולם ספורט</option>
-                    <option value="מגרש ספורט">מגרש ספורט</option>
-                    <option value="כדורגל">כדורגל</option>
-                    <option value="כדורסל">כדורסל</option>
-                    <option value="טניס">טניס</option>
-                </select>
-                <!-- the table element -->
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Map</th>
-                                <th>Street</th>
-                                <th>Number</th>
-                            </tr>
-                        </thead>
-                        <tbody id="table-body">
-                            <!-- Table rows with data here -->
-                        </tbody>
-                    </table>
+
+            <div class="row justify-content-around">
+                <div class="col-md-7">
+                    <div>
+                        <div style="height:500px;">
+                            <div id="map" class="w-100 h-100"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3 m-md-0">
+                    <!-- search city bar -->
+                    <div class="form-group">
+                        <label for="location-search"> Search city name in (Hebrew)</label>
+                        <select class="form-control" id="location-search" style="width: 100%">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <!-- search type bar -->
+                        <Label for="type-select">Select sport field type</Label>
+                        <select class="form-control" id="type-select" style="width: 100%">
+                            <option value="אולם ספורט">אולם ספורט</option>
+                            <option value="מגרש ספורט">מגרש ספורט</option>
+                            <option value="כדורגל">כדורגל</option>
+                            <option value="כדורסל">כדורסל</option>
+                            <option value="טניס">טניס</option>
+                        </select>
+                    </div>
+                    <!-- the table element -->
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Map</th>
+                                    <th>Street</th>
+                                    <th>Number</th>
+                                </tr>
+                            </thead>
+                            <tbody id="table-body">
+                                <!-- Table rows with data here -->
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -197,6 +217,22 @@
                 },
             })
         });
+
+
+        // Set up the map
+        var map = L.map('map').setView([31.80309338, 35.10942674], 17);
+
+        // Add the tile layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        }).addTo(map);
+
+        // Define the coordinate to display
+        var coordinate = [31.80309338, 35.10942674];
+
+        // Add a marker for the coordinate to the map
+        L.marker(coordinate).addTo(map);
+    </script>
+
 
     </script>
 </body>
