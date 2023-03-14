eed09a87f6eff3175cba93d69ec55b93752b4a21
rotem <bgdogname@gmail.com>
2023-03-14 08:04:01 +0200

    apply new inputs

--- reservefield.php
+++ reservefield.php
@@ -22,11 +22,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
         integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
         integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
 </head>
 
 <body>
@@ -80,36 +83,62 @@
         <div class="container">
 
             <div class="row justify-content-around">
-                <div class="col-md-6">
+                <div class="col-md-5">
                     <div>
                         <div style="height:500px;">
                             <div id="map" class="w-100 h-100"></div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5 mt-3 m-md-0">
+                <div class="col-md-6 mt-3 m-md-0">
                     <!-- search city bar -->
-                    <div class="form-group">
-                        <label for="location-search"> Search city name in (Hebrew)</label>
-                        <select class="form-control" id="location-search" style="width: 100%">
-                        </select>
+                    <div class="row">
+                        <div class="form-group col-6 col-md-7">
+                            <label for="location-search"> Search city name in Hebrew:</label>
+                            <select class="form-control" id="location-search" style="width: 100%">
+                            </select>
+                        </div>
+                        <div class="form-group col-6 col-md-5">
+                            <!-- search type bar -->
+                            <Label for="type-select">Select sport field type:</Label>
+                            <select class="form-control" id="type-select" style="width: 100%">
+                                <option value="אולם ספורט">אולם ספורט</option>
+                                <option value="מגרש ספורט">מגרש ספורט</option>
+                                <option value="כדורגל">כדורגל</option>
+                                <option value="כדורסל">כדורסל</option>
+                                <option value="טניס">טניס</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <!-- search type bar -->
-                        <Label for="type-select">Select sport field type</Label>
-                        <select class="form-control" id="type-select" style="width: 100%">
-                            <option value="אולם ספורט">אולם ספורט</option>
-                            <option value="מגרש ספורט">מגרש ספורט</option>
-                            <option value="כדורגל">כדורגל</option>
-                            <option value="כדורסל">כדורסל</option>
-                            <option value="טניס">טניס</option>
-                        </select>
+                    <div class="row">
+                        <div class="form-group col-6 col-md-7">
+                            <!-- search type bar -->
+                            <Label for="type-select">Choose date:</Label> <input type="text" id="datepicker"
+                                class="form-control" autocomplete="off">
+                        </div>
+                        <div class="form-group col-6 col-md-3">
+                            <!-- search type bar -->
+                            <Label for="type-select">Select Time:</Label>
+                            <select class="form-control col" aria-label="Select Time" id="selecttime">
+                                <option value="">--Select Time--</option>
+                                <option value="10:00 AM">10:00 AM</option>
+                                <option value="11:30 AM">11:30 AM</option>
+                                <option value="1:00 PM">1:00 PM</option>
+                                <option value="2:30 PM">2:30 PM</option>
+                                <option value="4:00 PM">4:00 PM</option>
+                                <option value="5:30 PM">5:30 PM</option>
+                                <option value="7:00 PM">7:00 PM</option>
+                                <option value="8:30 PM">8:30 PM</option>
+                                <option value="10:00 PM">10:00 PM</option>
+                            </select>
+                        </div>
                     </div>
                     <!-- the table element -->
-                    <div class="table-responsive" style="max-height: 300px;">
+                    <div class="table-responsive" style="max-height: 300px;height: 300px;">
                         <table class=" table table-striped">
                             <thead>
                                 <tr>
+                                    <th>Check Availablity</th>
                                     <th>Map</th>
                                     <th>Lighting</th>
                                     <th>Accessible</th>
@@ -121,9 +150,13 @@
                             </tbody>
                         </table>
                     </div>
+                    <div class="col-md-6 offset-md-3 text-center mt-2">
+
+                        <button class="btn btn-success btn-lg mb-2 mt-2">Reserve</button>
+                    </div>
                 </div>
             </div>
-        </div>
+
     </main>
 
     <footer class=" container">
@@ -154,7 +187,8 @@
                                 const latitude = row.latitude;
                                 const longitude = row.longitude;
 
-                                var selectButton = $('<button>').addClass('btn btn-primary').text('Select');
+                                var CheckButton = $('<button>').addClass('btn btn-primary').text('Check');
+                                var selectButton = $('<button>').addClass('btn btn-secondary').text('Show');
                                 selectButton.click(function () {
                                     changeCoords(latitude, longitude)
                                 });
@@ -162,6 +196,7 @@
                                 const suitable_for_the_disabled = row.suitable_for_the_disabled;
                                 const parking = row.parking;
 
+                                tr.append($('<td>').append(CheckButton));
                                 tr.append($('<td>').append(selectButton));
                                 tr.append($('<td>').text(lighting));
                                 tr.append($('<td>').text(suitable_for_the_disabled));
@@ -234,7 +269,42 @@
 
             // Center the map on the new coordinates
             map.setView([lat, lng], 17);
+
+            // setting the min date to today
+            $(document).ready(function () {
+                var today = new Date();
+                var year = today.getFullYear();
+                var month = today.getMonth() + 1;
+                var day = today.getDate();
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                if (day < 10) {
+                    day = '0' + day;
+                }
+                var minDate = year + '-' + month + '-' + day;
+                $('input[type="date"]').attr('min', minDate);
+            })
         }
+
+
+
+        $(document).ready(function () {
+            $('#selecttime').select2({
+                placeholder: 'Select time:',
+                allowClear: true,
+                theme: 'classic',
+            });
+        })
+        $(document).ready(function () {
+            var today = new Date();
+            // Initialize Datepicker
+            $('#datepicker').datepicker({
+                format: 'dd-mm-yyyy',
+                orientation: 'bottom',
+                startDate: today
+            });
+        });
     </script>
 </body>
 
