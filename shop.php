f0f3fb91c1d48e6844cbf8b66e53b33cb0324222
omershik <omer41198@gmail.com>
2023-03-16 15:58:03 +0200

    change the shop

---
+++ shop.php
@@ -0,0 +1,44 @@
+<?php
+$host = "localhost"; // database host
+$username = "test"; // database username
+$password = "12345"; // database password
+$dbname = "sadna"; // database name
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // Output container div
+    echo "<div class='product-container'>";
+    
+    // Output data of each row
+    $count = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        // Output product div with data
+        echo "<div class='product'>";
+        echo "<img src='" . $row["picture"] . "' alt='" . $row["name"] . "'>";
+        echo "<h2>" . $row["name"] . "</h2>";
+        echo "<p>" . $row["content"] . "</p>";
+        echo "<p>Price: " . $row["price"] . "</p>";
+        echo "</div>";
+        
+        // Increment count and check if we need to start a new row
+        $count++;
+        if ($count == 3) {
+            echo "<div class='clear'></div>";
+            $count = 0;
+        }
+    }
+    
+    // Close container div
+    echo "</div>";
+}
+?>
---
+++ shophtml.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+    <div id="navbar"></div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    $(function(){
+        $("#navbar").load("navbar.html")
+        // $.get('navbar.html', function(data) {
+        // // Find all elements with class 'my-class'
+        // $(data).find('#store').addClass('font-weight-normal');
+        // });    
+    });
+    </script>
+    
+    <main role="main">
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="display-3">Hello, *user*</h1>
+                        <p><span class="font-weight-bold">It's good to see you here!</span>
+                            here you can purchase our products in cheap prices that can be use in any game that you like to play.<br>
+                            you can use our search bar right down or scroll down and see our products. 
+                        </p>
+                        <form class="d-flex ms-auto mt-2">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div class="col-md-6" style="padding-left: 90px;">
+                        <img src="gif.gif" width="400px" height="280px">
+                    </div>
+                </div>
+            <div>
+        </div>
+
+        <?php include 'shop.php'; ?>
+
+    </main>
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"> </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
+</html>
+</html>
