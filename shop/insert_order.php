b6ee64fbaf53e75bdd1c2e136c4f0694fe173f96
omershik <omer41198@gmail.com>
2023-04-14 16:52:10 +0300

    CREATE ORDERS TABLE AND FILES

---
+++ shop/insert_order.php
@@ -0,0 +1,37 @@
+<?php
+include '../connection.php';
+
+
+// Retrieve product information from AJAX request
+$productId = $_GET['productId'];
+$productName = $_GET['productName'];
+$quantity = $_GET['quantity'];
+$username = $_GET['username'];
+
+
+// Get product details from database
+$sql = "SELECT * FROM products WHERE product_id = $productId";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $productName = $row['name'];
+    $productPicture = base64_encode($row['picture']);
+    $productPrice = $row['price'];
+    $dateTime = date("Y-m-d H:i:s");
+
+    // Insert order details into orders table
+    $sql = "INSERT INTO shop_reservations (username, product, picture, price, quantity, date_time) 
+            VALUES ('$username', '$productName', '$productPicture', '$productPrice', '$quantity', '$dateTime')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Order placed successfully.";
+    } else {
+        echo "Failed to place order. Error: " . $conn->error;
+    }
+} else {
+    echo "Failed to retrieve product details.";
+}
+
+$conn->close();
+?>
