33b4d9147c85b8924731694e97cea1b09ca80da2
rotem <bgdogname@gmail.com>
2023-04-20 15:46:16 +0300

    added logo

--- player/playerpage.php
+++ player/playerpage.php
@@ -61,7 +61,7 @@
     }
     ?>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="navbar-brand">4Play</div>
+        <div class="navbar-brand"><img src="http://localhost/Sadna/images/4Play.PNG" width="70"></div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- try.php
+++ try.php
@@ -1,42 +1,32 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <title>Example Page</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
+    <title>PayPal Payments Standard Example</title>
+    <script src="https://www.paypal.com/sdk/js?client-id=AY6bFReVUVISpKrLF1_LaVgud8umuLQr7lvOLjus3soqaTf_fZgZwTQE5hyUZ4Xw7I-u_9CcTL1QPZpJ&currency=USD"></script>
 </head>
+
 <body>
-    <select id="product-search" style="width:100%;"></select>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <div id="paypal-button-container"></div>
     <script>
-        $(document).ready(function () {
-            $('#product-search').select2({
-                theme: "classic",
-                placeholder: "Search product",
-                ajax: {
-                    url: 'http://localhost/Sadna/shop/retrieve_product.php',
-                    dataType: 'json',
-                    delay: 250,
-                    data: function (params) {
-                        return {
-                            q: params.term
-                        };
-                    },
-                    processResults: function (data) {
-                        return {
-                            results: $.map(data, function (item) {
-                                return {
-                                    text: item.name,
-                                    id: item.name
-                                }
-                            })
-                        };
-                    },
-                    allowClear: true
-                },
-            })
-        });
+        paypal.Buttons({
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: '10.00'
+                        }
+                    }]
+                });
+            },
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(details) {
+                    alert('Transaction completed by ' + details.payer.name.given_name + '!');
+                });
+            }
+        }).render('#paypal-button-container');
     </script>
+
 </body>
+
 </html>
