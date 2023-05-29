626af360f9451698f9aa20898fe05a00ada70778
omershik <omer41198@gmail.com>
2023-05-29 14:23:39 +0300

    more issues fixed

--- shop/shophtml.php
+++ shop/shophtml.php
@@ -94,8 +94,6 @@
                             You can use our search bar right down or scroll down and see our products.
                         </p>
                         <div class="d-flex ms-auto mt-2 mb-2" method="POST" action=''>
-                            <!-- <select class="form-control" id="product-search" style="width: 100%"> 
-                        </select> -->
                             <input class="form-control me-2" type="search" name="char" id="search-input" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" id="search-btn" type="submit" name="submit"><svg id="svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -286,6 +284,7 @@
             });
 
         }
+        
 
         //end of document ready
     });
